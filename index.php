141bb37a76bc9a10c7b157daf01cc143cf947b9e
Ashish Antil <ashishantil.antil07@gmail.com>
2022-08-29 17:53:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,118 @@
-<php
-     include('.\\Main\\special.htm')
-     ?>
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Ashish Antil</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="special.css">
+  </head>
+  <body>
+
+    <div class="navBar">
+      <h3>Ashish Antil</h3>
+      <div class="navigator">
+        <section>
+          <div class="series">01.</div>
+          <a href="#Home">Home</a>
+        </section>
+        <section>
+          <div class="series">02.</div>
+          <a href="#About">About</a>
+        </section>
+        <section>
+          <div class="series">03.</div>
+          <a href="#Education_Status">Education Status</a>
+        </section>
+        <section>
+          <div class="series">04.</div>
+          <a href="#Achievements">Achievements</a>
+        </section>
+      </div>
+    </div>
+
+    <div class="main" id='Home'>
+      <div class="home">
+        <div class="picture">
+          <img src="../Sketchboard/Pictures/Text-cursor.png" alt="Ashish Antil">
+          <div class="pictureFrame"></div>
+          <div class="pictureFrame"></div>
+        </div>
+        <div class="details">
+          <div class="name">Ashish Antil</div>
+          <div class="description">An experienced Programmer at Microsoft.</div>
+          <div class="links">
+            <a href="#About">About</a>
+            <a href="#Achievements">Achievements</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="main" id='About'>
+      <h1 heading>About</h1>
+      <div class="container">
+        <section>
+          <h2 heading>General</h2>
+          <div class="content"></div>
+        </section>
+        <section>
+          <h2 heading>Employable</h2>
+          <div class="content"></div>
+        </section>
+      </div>
+    </div>
+
+    <div class="main" id='Education_Status'>
+      <h1 heading>Education Status</h1>
+      <div class="container">
+        <section>
+          <h2 heading>Schooling</h2>
+          <div class="content"></div>
+        </section>
+        <section>
+          <h2 heading>Graduation</h2>
+          <div class="content"></div>
+        </section>
+        <section>
+          <h2 heading>Employment</h2>
+          <div class="content"></div>
+        </section>
+      </div>
+    </div>
+
+    <div class="main" id='Achievements'>
+      <h1 heading>Achievements</h1>
+      <div class="container">
+        <section></section>
+        <section></section>
+        <section></section>
+      </div>
+    </div>
+
+    <footer>
+      <section class="maker">
+        <div>Made with ❤ by&nbsp;</div>
+        <a github href="https://github.com/AshishAntil07">Ashish</a>
+      </section>
+      <div class="navigator">
+        <section>
+          <a href="#Home">Home</a>
+        </section>
+        <section>
+          <a href="#About">About</a>
+        </section>
+        <section>
+          <a href="#Education_Status">Education Status</a>
+        </section>
+        <section>
+          <a href="#Achievements">Achievements</a>
+        </section>
+      </div>
+    </footer>
+    
+    <script src="special.js" async defer></script>
+  </body>
+</html>
